4586bd0494635a027ca2aee16016b1f7ae50bc07
EstebanMarin <estebanmarin002@gmail.com>
2024-10-25 22:49:53 -0500

    initial

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    typelevel-nix.url = "github:typelevel/typelevel-nix";
+    nixpkgs.follows = "typelevel-nix/nixpkgs";
+    flake-utils.follows = "typelevel-nix/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ typelevel-nix.overlays.default ];
+        };
+      in {
+        devShell = pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevelShell ];
+          name = "dreamwork-shell";
+          typelevelShell = {
+            jdk.package = builtins.getAttr "jdk${pkgs.lib.fileContents ./.java-version}" pkgs;
+          };
+        };
+      });
+}
